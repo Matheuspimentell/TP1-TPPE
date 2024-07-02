7dd3fa153dfd0649675f97f307643895e0ba68a7
Lude Ribeiro <ludeyuri07@gmail.com>
2024-07-02 12:17:27 -0300

    Improve tests folowing TDD method.

--- varejao/src/test/java/varejao/objects/ProductTest.java
+++ varejao/src/test/java/varejao/objects/ProductTest.java
@@ -1,8 +1,16 @@
-package objects.varejao;
+package varejao.objects;
 
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class ProductTest {
-  
+  @Test
+  public void createProduct() {
+    Product product = new Product("001", "Laptop", 1500.0, "unit");
+
+    assertEquals("001", product.getItemCode());
+    assertEquals("Laptop", product.getDescription());
+    assertEquals(1500.0, product.getSalePrice(), 0.001);
+    assertEquals("unit", product.getUnit());
+  }
 }
