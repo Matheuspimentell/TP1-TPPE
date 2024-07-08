0d77252cdd195d6e35436b635d695d048f2fb79e
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2024-07-07 23:28:06 -0300

    Added special customer verification

--- varejao/src/test/java/varejao/objects/CustomerTest.java
+++ varejao/src/test/java/varejao/objects/CustomerTest.java
@@ -3,8 +3,10 @@ package varejao.objects;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -48,5 +50,26 @@ public class CustomerTest {
     assertEquals(username, customer.getUsername());
     assertEquals(type, customer.getType());
     assertEquals(address, customer.getAddress());
+  }
+
+  @Test
+  public void verifySpecialEligibility() {
+    customer = new Customer(username, type, address);
+
+    List<Sale> orders = customer.getOrders();
+
+    LocalDateTime now = LocalDateTime.now();
+    double total = 0.0;
+
+    for (int i = 0; i < orders.size(); i++) {
+      LocalDateTime orderDate = orders.get(i).getDate();
+      if (orderDate.isAfter(now.minusMonths(1)) && orderDate.isBefore(now)) {
+        total += orders.get(i).getTotal();
+      }
+    }
+
+    boolean isSpecialEligible = total > 100.0;
+
+    assertEquals(isSpecialEligible, customer.isSpecialEligible());
   }
 }
