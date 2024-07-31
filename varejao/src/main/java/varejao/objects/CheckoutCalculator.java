81cbf63c1e76ee349c5f2633db97d8788200dcfa
Matheus Pimentel Leal <matheusp.eng@gmail.com>
2024-07-30 23:53:19 -0300

    Replace Sale::checkout() method with object method, still in need of testing.

---
+++ varejao/src/main/java/varejao/objects/CheckoutCalculator.java
@@ -0,0 +1,43 @@
+package varejao.objects;
+
+public class CheckoutCalculator {
+
+  private double icms;
+  private double shippingCost;
+  private double municipalTax;
+  private double generatedCashback;
+  private Customer customer;
+  private Address address;
+
+  public CheckoutCalculator(Sale sale) {
+    this.generatedCashback = sale.getCashback();
+    this.customer = sale.getCustomer();
+    this.address = this.customer.getAddress();
+    this.icms = sale.getICMS(this.address);
+    this.shippingCost = sale.getShippingCost(this.address);
+    this.municipalTax = sale.getMunicipalTax(this.address);
+  }
+
+  /**
+   * Calculates and returns checkout amount for the current sale.
+   *
+   * @param usedCashback  the cashback amount that the customer will use in this checkout
+   * @return  returns the sale checkout total amount of the sale
+  */
+  public double checkout(double usedCashback) {
+    this.shippingCost = customer.getType() == CustomerType.Prime ? 0.0 : shippingCost; // If customer is a Prime customer, no shipping cost
+    this.shippingCost = customer.getType() == CustomerType.Special ? shippingCost*0.7 : shippingCost; // If customer is a Special user, 30% off the shipping cost
+
+    double total += shippingCost+icms+municipalTax;
+    total -= usedCashback; // remove the cashback amount the user wants to use in this checkout
+
+    // Remove the cashback the user used in this checkout, and then add the generated cashback for this checkout
+    if (cashback > 0.0) {
+      customer.setCashback(customer.getCashback()-cashback);
+      customer.setCashback(getCashback());
+    }
+    
+    customer.addOrder(this); // Add the order to the customer's order list
+    return total;
+  }
+}
